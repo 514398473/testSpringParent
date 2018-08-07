93ffafbaa0cc3227d8c8dac520752b1bc768c481
xuzheng <xuzheng@tusvn.com>
2018-08-07 18:11:36 +0800

    添加测试工程

--- testSpringSericeTurbine/src/main/java/com/xz/springboot/Application.java
+++ testSpringSericeTurbine/src/main/java/com/xz/springboot/Application.java
@@ -1,6 +1,5 @@
 package com.xz.springboot;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -8,36 +7,21 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
 @EnableDiscoveryClient
+@RestController
 @EnableHystrix
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+@EnableTurbine
 public class Application {
-
-	@Value("${server.port}")
-	String port;
-
-	@RequestMapping("/hi")
-	@HystrixCommand(fallbackMethod = "hiError")
-	public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
-		return "hi " + name + " ,i am from port:" + port;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	public String hiError(String name) {
-		return "hi," + name + ",sorry,error!";
 	}
 
 }
